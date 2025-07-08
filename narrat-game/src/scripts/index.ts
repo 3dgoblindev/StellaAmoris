60da25626c70888f1091d9aa4725508633f2de3c
3DGoblinDev <lorenax230403@gmail.com>
2025-07-08 16:45:38 +0200

    sdf

---
+++ narrat-game/src/scripts/index.ts
@@ -0,0 +1,11 @@
+import game from './game.narrat';
+import tests from './tests.narrat';
+import cap1 from './cap1.narrat';
+import cap2 from './cap2.narrat';
+import cap3 from './cap3.narrat';
+import cap4 from './cap4.narrat';
+import cap5 from './cap5.narrat';
+import fin from './fin.narrat';
+
+
+export default [game, tests, cap1, cap2, cap3, cap4, cap5, fin];
